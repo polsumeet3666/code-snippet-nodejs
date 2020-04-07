ffcd1b92e8a9e744c4bbb3031d0780f16304e23f
Sumeet Pol <polsumeet3666@gmail.com>
2020-04-07 14:11:06 +0530

    added expres module and certs

--- saml2/app.js
+++ saml2/app.js
@@ -12,8 +12,8 @@ app.use(
 // Create service provider
 var sp_options = {
 	entity_id: "https://sp.example.com/metadata.xml",
-	private_key: fs.readFileSync("key-file.pem").toString(),
-	certificate: fs.readFileSync("cert-file.crt").toString(),
+	private_key: fs.readFileSync("./certs/key-file1.pem").toString(),
+	certificate: fs.readFileSync("./certs/cert-file1.crt").toString(),
 	assert_endpoint: "https://sp.example.com/assert",
 };
 var sp = new saml2.ServiceProvider(sp_options);
@@ -23,8 +23,8 @@ var idp_options = {
 	sso_login_url: "https://idp.example.com/login",
 	sso_logout_url: "https://idp.example.com/logout",
 	certificates: [
-		fs.readFileSync("cert-file1.crt").toString(),
-		fs.readFileSync("cert-file2.crt").toString(),
+		fs.readFileSync("./certs/cert-file1.crt").toString(),
+		fs.readFileSync("./certs/cert-file2.crt").toString(),
 	],
 };
 var idp = new saml2.IdentityProvider(idp_options);
